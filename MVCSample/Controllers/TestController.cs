472bb311e6bb8afe101071d2d1959b29f485933a
MishraSaurabh <saurabhmishra22@gmail.com>
2016-04-11 20:30:57 -0500

    MVC5Sample
    MVC5Sample

---
+++ MVCSample/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSample.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test        
+        public string GetString()
+        {
+            return "This is Hello from Test Controller";
+        }
+
+        public  ActionResult GetView()
+        {
+            return View("MyView");
+       }
+
+        public ActionResult GetSharedView()
+        {
+            return View("SharedView");
+        }
+    }
+}
