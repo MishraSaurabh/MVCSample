5d51a7ab6dfc4903dd9a7994bd0f364b65f0985e
MishraSaurabh <saurabhmishra22@gmail.com>
2016-04-11 22:08:42 -0500

    Employee model

---
+++ MVCSample/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSample.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeID { get; set; }
+        public int Salary { get; set; }
+    }
+}
