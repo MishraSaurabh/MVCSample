a51012529c9ea5c52a8da96ca5b0f0b84a617d3b
MishraSaurabh <saurabhmishra22@gmail.com>
2016-04-12 22:03:15 -0500

    Changes to integrate EF 6.1

--- MVCSample/Models/Employee.cs
+++ MVCSample/Models/Employee.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCSample.Models
 {
     public class Employee
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        [Key]
         public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
         public int Salary { get; set; }
     }
 }
